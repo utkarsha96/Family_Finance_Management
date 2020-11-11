5c421af4196610ad96cd6b2602de8f1999b9798d
Mrunal Tupe <mrunal4271@gmail.com>
2020-11-11 20:17:19 +0530

    IncomeActivity

---
+++ app/src/main/java/com/example/android/familyfinancemanagement/ExpensesActivity.java
@@ -0,0 +1,6 @@
+package com.example.android.familyfinancemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpensesActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/android/familyfinancemanagement/IncomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.familyfinancemanagement;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IncomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income);
+
+    }
+}
